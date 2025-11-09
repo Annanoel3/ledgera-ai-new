a5cfb3b98abc89c78162c20de14188d869809dd4
Anna Ballew <annanoelwenballew@gmail.com>
2025-11-09 04:54:09 +0000

    File changes

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1001,7 +1001,7 @@ export default function Chat() {
                           <li style={{ marginBottom: '0.25rem' }}>Catch costs you might have missed</li>
                           <li style={{ marginBottom: '0.25rem' }}>Generate reports and ROI analysis</li>
                           <li style={{ marginBottom: '0.25rem' }}>Prepare for tax season</li>
-                          <li style={{ marginBottom: '0.25rem' }>Answer questions about your business</li>
+                          <li style={{ marginBottom: '0.25rem' }}>Answer questions about your business</li>
                         </ul>
                         <p style={{
                       fontSize: '15px',
